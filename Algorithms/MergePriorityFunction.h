ca3d09af38a7acb9a38abaa6f94b2ca2229af5ed
Stephen Plaza <plaza.stephen@gmail.com>
2012-08-27 01:19:35 -0400

    makes agglomeratio much faster by lazily updating probabilities

--- Algorithms/MergePriorityFunction.h
+++ Algorithms/MergePriorityFunction.h
@@ -3,21 +3,23 @@
 
 #include "../FeatureManager/FeatureManager.h"
 #include "../DataStructures/Rag.h"
+#include <tr1/unordered_set>
+#include "../DataStructures/AffinityPair.h"
 
 namespace NeuroProof {
 
 class MergePriority {
   public:
-    MergePriority(FeatureMgr* feature_mgr_, Rag<unsigned int>* rag_) : 
+    MergePriority(FeatureMgr* feature_mgr_, Rag<Label>* rag_) : 
                         feature_mgr(feature_mgr_), rag(rag_) {} 
 
     virtual void initialize_priority(double threshold) = 0;
 
-    virtual RagEdge<unsigned int>* get_top_edge() = 0;
+    virtual RagEdge<Label>* get_top_edge() = 0;
 
-    virtual void add_dirty_edge(RagEdge<unsigned int>* edge) = 0;
+    virtual void add_dirty_edge(RagEdge<Label>* edge) = 0;
 
-    bool valid_edge(RagEdge<unsigned int>* edge)
+    bool valid_edge(RagEdge<Label>* edge)
     {
         if (edge->is_preserve() || edge->is_false_edge()) {
             return false;
@@ -28,22 +30,23 @@ class MergePriority {
     virtual bool empty() = 0;
 
   protected:
-    Rag<unsigned int>* rag;
+    Rag<Label>* rag;
     FeatureMgr* feature_mgr;
 
 };
 
 class ProbPriority : public MergePriority {
   public:
-    ProbPriority(FeatureMgr* feature_mgr_, Rag<unsigned int>* rag_) :
+    ProbPriority(FeatureMgr* feature_mgr_, Rag<Label>* rag_) :
                     MergePriority(feature_mgr_, rag_), Epsilon(0.00001) {}
 
     void initialize_priority(double threshold_)
     {
         threshold = threshold_;
-        for (Rag<unsigned int>::edges_iterator iter = rag->edges_begin(); iter != rag->edges_end(); ++iter) {
+        for (Rag<Label>::edges_iterator iter = rag->edges_begin(); iter != rag->edges_end(); ++iter) {
             if (valid_edge(*iter)) {
                 double val = feature_mgr->get_prob(*iter);
+                (*iter)->set_weight(val);
 
                 if (val <= threshold) {
                     ranking.insert(std::make_pair(val, std::make_pair((*iter)->get_node1()->get_node_id(), (*iter)->get_node2()->get_node_id())));
@@ -51,14 +54,49 @@ class ProbPriority : public MergePriority {
             }
         }
     }
+    
+    void clear_dirty()
+    {
+        for (Dirty_t::iterator iter = dirty_edges.begin(); iter != dirty_edges.end(); ++iter) {
+            Label node1 = (*iter).region1;
+            Label node2 = (*iter).region2;
+            RagNode<Label>* rag_node1 = rag->find_rag_node(node1); 
+            RagNode<Label>* rag_node2 = rag->find_rag_node(node2); 
+
+            if (!(rag_node1 && rag_node2)) {
+                continue;
+            }
+            RagEdge<Label>* rag_edge = rag->find_rag_edge(rag_node1, rag_node2);
+
+            if (!rag_edge) {
+                continue;
+            }
+
+            assert(rag_edge->is_dirty());
+            rag_edge->set_dirty(false);
+           
+            if (valid_edge(rag_edge)) {
+                double val = feature_mgr->get_prob(rag_edge);
+                rag_edge->set_weight(val);
+
+                if (val <= threshold) {
+                    ranking.insert(std::make_pair(val, std::make_pair(node1, node2)));
+                }
+            }
+        }
+        dirty_edges.clear();
+    }
 
     bool empty()
     {
+        if (ranking.empty()) {
+            clear_dirty();
+        }
         return ranking.empty();
     }
 
 
-    RagEdge<unsigned int>* get_top_edge()
+    RagEdge<Label>* get_top_edge()
     {
         EdgeRank_t::iterator first_entry = ranking.begin();
         double curr_threshold = (*first_entry).first;
@@ -89,20 +127,26 @@ class ProbPriority : public MergePriority {
             return 0;
         }
 
-        double val = feature_mgr->get_prob(rag_edge);
+        double val = rag_edge->get_weight();
+
+        if (rag_edge->is_dirty()) {
+            val = feature_mgr->get_prob(rag_edge);
+            rag_edge->set_weight(val);
+            rag_edge->set_dirty(false);
+            dirty_edges.erase(OrderedPair<Label>(node1, node2));
+        }
+
         if (val > (curr_threshold + Epsilon)) {
             return 0;
         }
         return rag_edge; 
     }
 
-    void add_dirty_edge(RagEdge<unsigned int>* edge)
+    void add_dirty_edge(RagEdge<Label>* edge)
     {
         if (valid_edge(edge)) {
             edge->set_dirty(true);
-            double val = feature_mgr->get_prob(edge);
-            ranking.insert(std::make_pair(val, std::make_pair((edge)->get_node1()->get_node_id(), (edge)->get_node2()->get_node_id())));
-            edge->set_dirty(false);
+            dirty_edges.insert(OrderedPair<Label>(edge->get_node1()->get_node_id(), edge->get_node2()->get_node_id()));
         }
     }
 
@@ -110,8 +154,10 @@ class ProbPriority : public MergePriority {
 
     double threshold;
     const double Epsilon;
-    typedef std::multimap<double, std::pair<unsigned int, unsigned int> > EdgeRank_t; 
+    typedef std::multimap<double, std::pair<Label, Label> > EdgeRank_t; 
+    typedef std::tr1::unordered_set<OrderedPair<Label>, OrderedPair<Label> > Dirty_t; 
     EdgeRank_t ranking;
+    Dirty_t dirty_edges;
 
 };
 
