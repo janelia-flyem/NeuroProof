8210cd378365fc1824d8ef2b3426238ee50c6820
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-15 20:21:47 -0400

    configure-for-conda.sh: If new install location differs from most recent install location, remove the old build directory.

--- configure-for-conda.sh
+++ configure-for-conda.sh
@@ -3,13 +3,25 @@ if [ "$#" -ne 1 ]; then
     exit 1;
 fi
 
-# Start in the same directory as this script.
-cd `dirname $0`
-
-export PREFIX="$1"
+export PREFIX="${1%/}"
 export PYTHON="${PREFIX}/bin/python"
 export CPU_COUNT=`python -c "import multiprocessing; print multiprocessing.cpu_count()"`
 export PATH="${PREFIX}/bin":$PATH
+
+# Start in the same directory as this script.
+cd `dirname $0`
+
+# If the build dir already exists and CMAKE_INSTALL_PREFIX doesn't
+# match the new destination, we need to start from scratch.
+if [[ -e build/CMakeCache.txt ]]; then
+
+    grep "CMAKE_INSTALL_PREFIX:PATH=$PREFIX" build/CMakeCache.txt > /dev/null 2> /dev/null
+    GREP_RESULT=$?
+    if [[ $GREP_RESULT == 1 ]]; then
+        echo "*** Removing old build directory: $(pwd)/build" 2>&1
+        rm -r build
+    fi
+fi
 
 # Install gcc first if necessary.
 if [ ! -e "${PREFIX}/bin/gcc" ]; then
