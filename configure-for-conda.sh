2626bb87ae5065c59389ccccf7ebf9b1adbb877e
Stuart Berg <bergs@janelia.hhmi.org>
2016-07-27 15:58:37 -0400

    configure-for-conda.sh: build.sh can be used locally now, instead of downloaded from github.

--- configure-for-conda.sh
+++ configure-for-conda.sh
@@ -29,16 +29,4 @@ if [ ! -e "${PREFIX}/bin/gcc" ]; then
     conda install --quiet -y -p "${PREFIX}" -c flyem gcc
 fi
 
-# Do you already have curl?
-curl --help > /dev/null
-if [[ $? == 0 ]]; then
-    CURL=curl
-else
-    # Install to conda prefix
-    echo "Installing curl..."
-    conda install --quiet -y -p "${PREFIX}" curl
-    CURL="${PREFIX}/bin/curl"
-fi
-
-BUILD_SCRIPT_URL=https://raw.githubusercontent.com/janelia-flyem/flyem-build-conda/master/neuroproof/build.sh
-"$CURL" "$BUILD_SCRIPT_URL" | bash -x -e -s - --configure-only
+bash -x -e conda-recipe/build.sh --configure-only
