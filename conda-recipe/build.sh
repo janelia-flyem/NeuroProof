6c9eb8485ad32519e89098940e729a7f1378e19e
Stuart Berg <bergs@janelia.hhmi.org>
2017-08-02 13:43:18 -0400

    Support Python 3

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,14 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+    CXXFLAGS="-stdlib=libc++"
 else
     DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
+    CXXFLAGS=""
 fi
 
 CONFIGURE_ONLY=0
@@ -15,22 +21,28 @@ if [[ $1 != "" ]]; then
     fi
 fi
 
+PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
+PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
+PY_ABI=${PY_VER}${PY_ABIFLAGS}
+
+
 # CONFIGURE
 mkdir -p build # Using -p here is convenient for calling this script outside of conda.
 cd build
 cmake ..\
-        -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
-        -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
+        -DCMAKE_C_COMPILER="${CC}" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DCMAKE_PREFIX_PATH="${PREFIX}" \
-        -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include -std=c++11" \
+        -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include -std=c++11 ${CXXFLAGS}" \
         -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DBOOST_ROOT=${PREFIX} \
         -DBoost_LIBRARY_DIR="${PREFIX}/lib" \
         -DBoost_INCLUDE_DIR="${PREFIX}/include" \
         -DPYTHON_EXECUTABLE="${PYTHON}" \
-        -DPYTHON_LIBRARY="${PREFIX}/lib/libpython2.7.${DYLIB_EXT}" \
-        -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python2.7" \
+        -DPYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_ABI}.${DYLIB_EXT}" \
+        -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python${PY_ABI}" \
         -DLIBDVIDCPP_INCLUDE_DIR="${PREFIX}/include" \
         -DLIBDVIDCPP_LIBRARY="${PREFIX}/lib/libdvidcpp.${DYLIB_EXT}" \
         -DENABLE_GUI=1 \
