186a84e2f6006ecbd74bf430d3ee8600b9f23440
Stuart Berg <bergs@janelia.hhmi.org>
2017-08-03 14:34:08 -0400

    testpriority: More verbose failure messages

--- integration_tests/testpriority.py
+++ integration_tests/testpriority.py
@@ -1,15 +1,16 @@
 import libNeuroProofPriority as npp
 import sys
 
-status = npp.initialize_priority_scheduler(sys.argv[1], .1, .9, .5)
+graph_json = sys.argv[1]
+output_path = sys.argv[2]
+groundtruth_path = sys.argv[3]
 
-num_examined = 0
-
-if not status:
-    exit(1)
+status = npp.initialize_priority_scheduler(graph_json, .1, .9, .5)
+assert status, "initialize_priority_scheduler() failed."
 
 npp.set_edge_mode(0.1, 0.9, 0.5)
 
+num_examined = 0
 while npp.get_estimated_num_remaining_edges() > 0:
     priority_info = npp.get_next_edge()
     xyzlocation = priority_info.location
@@ -20,18 +21,15 @@ while npp.get_estimated_num_remaining_edges() > 0:
     npp.set_edge_result(priority_info.body_pair, False)
     num_examined += 1
 
-if num_examined != 610:
-    exit(1)
+assert num_examined == 610
 
-npp.export_priority_scheduler(sys.argv[2])
+npp.export_priority_scheduler(output_path)
 
-tempfile = open(sys.argv[2])
-groundfile = open(sys.argv[3])
+tempout = open(output_path).read()
+groundout = open(groundtruth_path).read()
 
-tempout = tempfile.read()
-groundout = groundfile.read()
-
-if tempout != groundout:
-    exit(1)
+assert tempout == groundout, \
+    "Computed output ({}) doesn't match golden output ({})."\
+    .format(output_path, groundtruth_path)
     
 print("SUCCESS")
