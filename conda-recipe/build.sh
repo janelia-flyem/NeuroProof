97357bd3323b5d8463593bbc353e4e02656e5294
Stuart Berg <bergs@janelia.hhmi.org>
2017-08-03 12:13:25 -0400

    conda-recipe: Run tests as part of the build step

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -49,13 +49,26 @@ cmake ..\
 ##
 
 if [[ $CONFIGURE_ONLY == 0 ]]; then
+
     # BUILD
-    if [[ `uname` == 'Darwin' ]]; then
-        make -j${CPU_COUNT} 2> >(python "${RECIPE_DIR}"/filter-macos-linker-warnings.py)
-    else
-        make -j${CPU_COUNT}
-    fi
+    make -j${CPU_COUNT}
+    
+    # TEST
+    (
+        set -e
+    
+        # (Since conda hasn't performed its link step yet, we must 
+        #  help the tests locate their dependencies via LD_LIBRARY_PATH)
+        if [[ $(uname) == Darwin ]]; then
+            export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib":"${DYLD_FALLBACK_LIBRARY_PATH}"
+        else
+            export LD_LIBRARY_PATH="$PREFIX/lib":"${LD_LIBRARY_PATH}"
+        fi
         
-    # "install" to the build prefix (conda will relocate these files afterwards)
+        # Run the tests
+        make test
+    )
+    
+    # INSTALL
     make install
 fi
